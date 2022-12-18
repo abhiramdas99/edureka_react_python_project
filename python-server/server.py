861205e73c4f02e3b301dc2f536cd3d0a3fbd617
abhiramdas99 <abhiramdas99@gmail.com>
2022-12-19 00:18:18 +0530

    welcome its 1st commit

--- python-server/server.py
+++ python-server/server.py
@@ -1,11 +1,33 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+from flask import jsonify
+from flask import flash, Request
 
 app=Flask(__name__)
 
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']='python_web_mysql'
+
+mysql=MySQL(app)
+
+
 #Members api route 
-@app.route("/members")
-def members():
-    return {"members": ["member1","member2", "member3"]}
+@app.route("/employees")
+def employees():
+    try:
+        cur=mysql.connection.cursor()
+        data=cur.execute("select * from employee_detail")
+        if data>0:
+            rows=cur.fetchall()
+            resp=jsonify({"employees":rows})
+            resp.status_code=200
+            return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
 
 if __name__=="__main__":
     app.run(debug=True)
